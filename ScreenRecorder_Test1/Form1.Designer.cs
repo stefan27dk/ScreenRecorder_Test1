353feac52229af6e686e9e207e978313cbbcaec4
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-07-04 21:16:18 +0200

    Multithreading Test

--- ScreenRecorder_Test1/Form1.Designer.cs
+++ ScreenRecorder_Test1/Form1.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.fps_label = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.Player_pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player_pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // fps_label
             // 
             this.fps_label.AutoSize = true;
-            this.fps_label.Location = new System.Drawing.Point(346, 181);
+            this.fps_label.Location = new System.Drawing.Point(523, 70);
             this.fps_label.Name = "fps_label";
             this.fps_label.Size = new System.Drawing.Size(35, 13);
             this.fps_label.TabIndex = 0;
@@ -44,22 +46,33 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(375, 255);
+            this.label1.Location = new System.Drawing.Point(523, 83);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
+            // 
+            // Player_pictureBox
+            // 
+            this.Player_pictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Player_pictureBox.Location = new System.Drawing.Point(65, 57);
+            this.Player_pictureBox.Name = "Player_pictureBox";
+            this.Player_pictureBox.Size = new System.Drawing.Size(397, 289);
+            this.Player_pictureBox.TabIndex = 2;
+            this.Player_pictureBox.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Player_pictureBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.fps_label);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player_pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +82,7 @@
 
         private System.Windows.Forms.Label fps_label;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Player_pictureBox;
     }
 }
 
--- ScreenRecorder_Test1/Form1.cs
+++ ScreenRecorder_Test1/Form1.cs
@@ -15,22 +15,46 @@ namespace ScreenRecorder_Test1
     public partial class Form1 : Form
     {
 
-
+        // TEST
         Stopwatch stopwatch = new Stopwatch(); 
+
+
+        // Recorder - Thread 1
         Thread record_Thread1;
 
+
+        //Recorder - Thread 2
         Thread record_Thread2;
+        
 
+
+        //Recorder - Thread 3
+        Thread record_Thread3; 
+        
+        //Recorder - Thread 4
+        Thread record_Thread4;  
+        
+        
+        
+        //Recorder - Thread 5
+        Thread record_Thread5;
+                      
+        
+
+        //Display Recording - Thread 
+        Thread display_recording_Thread;
+        bool ready_To_Display = false;
+
+
+
+        // FPS
         int current_FPS = 0;
-        int stop = 0;
+        int stop = 0; // TEST
 
-
-        System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
-
-
-
-        // Recording Size
-        Size CaptureSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+         
+        System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer(); // Test
+        Bitmap LastImage; // Last image for disposing 
+        Size CaptureSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height); // Recording Size
 
             
 
@@ -38,8 +62,10 @@ namespace ScreenRecorder_Test1
         Bitmap Frame_Bmp1;
 
 
+        
 
 
+        // TEST
         private void Timer_Settings()
         {
             timer1.Interval = 1000;
@@ -50,13 +76,14 @@ namespace ScreenRecorder_Test1
 
 
 
-
+        // TEST
         void Tick(object sender, EventArgs e)
         {
             stop = 1;
         }
 
         
+
 
         public Form1()
         {
@@ -71,18 +98,44 @@ namespace ScreenRecorder_Test1
         //LOAD
         private void Form1_Load(object sender, EventArgs e)
         {
-            Timer_Settings();
+            Timer_Settings(); // TEST
 
-
+            // Recording Thread 1
             record_Thread1 = new Thread(Update_FPS);
             record_Thread1.IsBackground = true;
             record_Thread1.Start();
 
-
-
+            // Recording Thread 2
             record_Thread2 = new Thread(Update_FPS);
             record_Thread2.IsBackground = true;
-            record_Thread2.Start();
+            record_Thread2.Start();  
+            
+            
+            // Recording Thread 3
+            record_Thread3 = new Thread(Update_FPS);
+            record_Thread3.IsBackground = true;
+            record_Thread3.Start();
+
+
+            // Recording Thread 4
+            record_Thread4 = new Thread(Update_FPS);
+            record_Thread4.IsBackground = true;
+            record_Thread4.Start();     
+            
+            
+            // Recording Thread 4
+            record_Thread5 = new Thread(Update_FPS);
+            record_Thread5.IsBackground = true;
+            record_Thread5.Start();
+
+
+            ////Display Recording Thread
+            //display_recording_Thread = new Thread(Display_Recording);
+            //display_recording_Thread.IsBackground = true;
+            //display_recording_Thread.Start();
+
+
+
 
         }
 
@@ -109,13 +162,13 @@ namespace ScreenRecorder_Test1
 
 
 
-             if(Is_First_Thread == true)
-             {
+            if (Is_First_Thread == true)
+            {
                 Thread.Sleep(10);
-             }
+            }
 
 
-              Is_First_Thread = true;
+            Is_First_Thread = true;
 
 
 
@@ -131,7 +184,7 @@ namespace ScreenRecorder_Test1
                     while(Is_At_Middle != true)
                     {
 
-                      Thread.Sleep(10);
+                      Thread.Sleep(1);
 
                     }
                 }
@@ -147,15 +200,47 @@ namespace ScreenRecorder_Test1
 
 
 
-
-                // Image::::::START::::::::
-                Frame_Bmp1 = new Bitmap(CaptureSize.Width, CaptureSize.Height); // BMP
+                ready_To_Display = false;
+               // Image::::::START::::::::
+               Frame_Bmp1 = new Bitmap(CaptureSize.Width, CaptureSize.Height); // BMP
                 Graphics g = Graphics.FromImage(Frame_Bmp1); // Graphics
-
-                Is_At_Middle = true;
-
+                Is_At_Middle = true;   
                 g.CopyFromScreen(Point.Empty, Point.Empty, CaptureSize); // Screen To BMP
-                // Image::::::END::::::::
+                                                                         // Image::::::END::::::::
+
+
+                ready_To_Display = true;
+
+
+                // Display::::::START:::::::
+                Invoke(new Action(() =>
+                {
+                    Player_pictureBox.BackgroundImage = Frame_Bmp1;
+                    Player_pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
+                }));
+                // Display::::::END:::::::
+
+
+
+
+
+
+
+               // CLEAN:::::::START:::::::
+
+                // Dispose Last Image
+                if (Frame_Bmp1 != LastImage && LastImage != null)
+                {
+                    LastImage.Dispose();
+                }
+
+               // Assign Last Image
+               LastImage = Frame_Bmp1;
+                g.Dispose();
+
+               // CLEAN:::::::END:::::::
+
+
 
 
 
@@ -177,14 +262,48 @@ namespace ScreenRecorder_Test1
 
 
 
+
                 Is_At_Middle = false;
                 Is_Thread_Working = false;
 
             }
 
-
             
         }
+
+
+
+        //private void Display_Recording()
+        //{
+        //    while(true)
+        //    {
+        //        Thread.Sleep(100);
+        //        Invoke(new Action(() =>
+        //        {
+        //            Player_pictureBox.BackgroundImage = LastImage;
+        //            Player_pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
+        //            Player_pictureBox.Refresh();
+        //        }));
+
+
+        //        //CLEAN:::::::START:::::::
+
+        //        // Dispose Last Image
+        //        if (Frame_Bmp1 != LastImage && LastImage != null)
+        //        {
+        //            LastImage.Dispose();
+        //        }
+
+        //        // Assign Last Image 
+        //        LastImage = Frame_Bmp1;
+             
+        //        //CLEAN:::::::END:::::::
+
+        //    }
+
+
+        //}
+
 
 
 
