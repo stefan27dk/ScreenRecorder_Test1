1b7e2f014ae9f4a2e03858b9fef94f48c446c96f
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-07-04 17:13:43 +0200

    Add project files.

---
+++ ScreenRecorder_Test1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ScreenRecorder_Test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fps_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fps_label
+            // 
+            this.fps_label.AutoSize = true;
+            this.fps_label.Location = new System.Drawing.Point(346, 181);
+            this.fps_label.Name = "fps_label";
+            this.fps_label.Size = new System.Drawing.Size(35, 13);
+            this.fps_label.TabIndex = 0;
+            this.fps_label.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(375, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fps_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fps_label;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ScreenRecorder_Test1/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenRecorder_Test1
+{
+    public partial class Form1 : Form
+    {
+
+
+        Stopwatch stopwatch = new Stopwatch(); 
+        Thread record_Thread1;
+
+        Thread record_Thread2;
+
+        int current_FPS = 0;
+        int stop = 0;
+
+
+        System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+
+
+
+        // Recording Size
+        Size CaptureSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            
+
+        // Captured Frame
+        Bitmap Frame_Bmp1;
+
+
+
+
+        private void Timer_Settings()
+        {
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(Tick);
+            timer1.Enabled = true;
+           
+        }
+
+
+
+
+        void Tick(object sender, EventArgs e)
+        {
+            stop = 1;
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        //LOAD
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer_Settings();
+
+
+            record_Thread1 = new Thread(Update_FPS);
+            record_Thread1.IsBackground = true;
+            record_Thread1.Start();
+
+
+
+            record_Thread2 = new Thread(Update_FPS);
+            record_Thread2.IsBackground = true;
+            record_Thread2.Start();
+
+        }
+
+
+
+
+
+
+        bool Is_First_Thread = false;
+
+        bool Is_Thread_Working = false;   // FIRST THREAD
+        bool Is_At_Middle = false;
+
+
+        private void Update_FPS()
+        {
+           
+            //TEST
+            Invoke(new Action(() =>
+            {
+                timer1.Start();
+
+            }));
+
+
+
+             if(Is_First_Thread == true)
+             {
+                Thread.Sleep(10);
+             }
+
+
+              Is_First_Thread = true;
+
+
+
+
+
+
+            while (stop!= 1) // TEST
+            {
+
+
+                if (Is_Thread_Working == true)
+                {
+                    while(Is_At_Middle != true)
+                    {
+
+                      Thread.Sleep(10);
+
+                    }
+                }
+
+
+
+                Is_Thread_Working = true; // First Thread
+
+
+                stopwatch.Start();   // TEST
+
+                current_FPS++;
+
+
+
+
+                // Image::::::START::::::::
+                Frame_Bmp1 = new Bitmap(CaptureSize.Width, CaptureSize.Height); // BMP
+                Graphics g = Graphics.FromImage(Frame_Bmp1); // Graphics
+
+                Is_At_Middle = true;
+
+                g.CopyFromScreen(Point.Empty, Point.Empty, CaptureSize); // Screen To BMP
+                // Image::::::END::::::::
+
+
+
+                //FPS
+                Invoke(new Action(() =>
+                {
+
+                    fps_label.Text = current_FPS.ToString();
+                }));
+
+
+
+                // FPS
+                Invoke(new Action(() =>
+                {
+                    stopwatch.Stop();
+                    label1.Text = stopwatch.ElapsedMilliseconds.ToString();
+                }));
+
+
+
+                Is_At_Middle = false;
+                Is_Thread_Working = false;
+
+            }
+
+
+            
+        }
+
+
+
+    }
+}
